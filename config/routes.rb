52975db58e499c026aebc304467380082c7359ae
hitoe <sousaku_0912@yahoo.co.jp>
2021-09-06 00:05:12 +0000

    close #1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  # どのcontroller, templateをレンダリングするか選択
+  
+  root 'static_pages#home'
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
-  root 'application#hello'
+  get 'static_pages/contact'
 end
