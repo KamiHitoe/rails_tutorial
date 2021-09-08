d3fdcb96a1778c674d44d80e0a6364312e01fc79
hitoe <sousaku_0912@yahoo.co.jp>
2021-09-08 14:11:21 +0000

    m helper

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -6,25 +6,25 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     get static_pages_home_url
     # HTTP status code: 200
     assert_response :success
-    assert_select "title", "Home"
+    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
-    assert_select "title", "Help"
+    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
-    assert_select "title", "About"
+    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get contact" do
     get static_pages_contact_url
     assert_response :success
-    assert_select "title", "Contact"
+    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
   end
 
 end
